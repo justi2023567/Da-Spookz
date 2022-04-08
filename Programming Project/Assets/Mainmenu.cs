25381b3b6687eaafedd5f2222b5c6512765a0c1d
justi2023567 <justi2023567@live.ytech.edu>
2022-04-08 08:36:55 -0400

    Main Menu buttons

--- Programming Project/Assets/Mainmenu.cs
+++ Programming Project/Assets/Mainmenu.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Mainmenu : MonoBehaviour
-{
+public class Mainmenu : MonoBehaviour {
     // Start is called before the first frame update
     public void PlayGame ()
     {
-        SceneManager.LoadScence(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
 }
