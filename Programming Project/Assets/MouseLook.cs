9db57cff0d2f3f41f6de4cf4d59364c08021a13d
justi2023567 <justi2023567@live.ytech.edu>
2022-04-12 10:14:34 -0400

    Updated Movement and added Gravity
    Added Gravity

--- Programming Project/Assets/MouseLook.cs
+++ Programming Project/Assets/MouseLook.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CamControl : MonoBehaviour
+public class MouseLook : MonoBehaviour
 {
   public float mouseSensitivity = 100f;
 
