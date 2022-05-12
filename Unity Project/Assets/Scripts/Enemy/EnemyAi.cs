91572d6bdf7265f90c41f80c0b6e064a69386e54
Jlara2005 <90395076+Jlara2005@users.noreply.github.com>
2022-05-12 10:24:53 -0400

    made the AI attack

--- Unity Project/Assets/Scripts/Enemy/EnemyAi.cs
+++ Unity Project/Assets/Scripts/Enemy/EnemyAi.cs
@@ -5,9 +5,9 @@ using UnityEngine.AI;
 
 public class EnemyAi : MonoBehaviour
 {
-
+    float timer = 0;
      public float lookRadius = 10f;
-
+     public GameObject health;
      Transform target;
      NavMeshAgent agent;
 
@@ -19,29 +19,18 @@ public class EnemyAi : MonoBehaviour
 
      void Update()
      {
+       timer += Time.deltaTime;
        float distance = Vector3.Distance(target.position, transform.position);
 
        if (distance <= lookRadius)
        {
          agent.SetDestination(target.position);
 
-         if (distance <= agent.stoppingDistance)
+         if (distance <= agent.stoppingDistance && timer >= 0.75f)
          {
-           FaceTarget();
+           health.GetComponent<PlayerHealth>().TakeDamage(4); // the number in take
+           timer = 0;
          }
       }
     }
-
-    void FaceTarget()
-    {
-      Vector3 direction = (target.position - transform.position).normalized;
-      Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
-      transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
-    }
-
-     void OnDrawGizmosSelected()
-     {
-       Gizmos.color = Color.red;
-       Gizmos.DrawWireSphere(transform.position, lookRadius);
-     }
   }
