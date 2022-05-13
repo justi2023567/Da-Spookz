83680801fac6f2b77742631610f7f1084a847439
justi2023567 <justi2023567@live.ytech.edu>
2022-05-13 14:16:06 -0400

    Files Fix

--- Unity Project/Assets/Scripts/Enemy/EnemyAi.cs
+++ Unity Project/Assets/Scripts/Enemy/EnemyAi.cs
@@ -33,18 +33,4 @@ public class EnemyAi : MonoBehaviour
          }
       }
     }
-
-    void FaceTarget()
-    {
-      Vector3 direction = (target.position - transform.position).normalized;
-      Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
-      transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
-    }
-
-     void OnDrawGizmosSelected()
-     {
-       Gizmos.color = Color.red;
-       Gizmos.DrawWireSphere(transform.position, lookRadius);
-     }
-
-}
+  }
